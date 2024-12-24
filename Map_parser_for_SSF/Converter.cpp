26f62af29b5205b3a0413381f03cd62e6b1eb919
Владислав Иванишин <ivanishin.vladislav@gmail.com>
2024-12-25 01:06:47 +0300

    Added filename check for non ascii symbols. Removing them from the filename.

--- Map_parser_for_SSF/Converter.cpp
+++ Map_parser_for_SSF/Converter.cpp
@@ -150,9 +150,14 @@ void Converter::convertMap(const std::filesystem::path& filepath)
 	}
 	const std::string rawData = gzip::decompress(zipData.data(), zipData.size());
 
+	// trying to bypass germans umlauts in map file names
+	std::wstring wfilepath = filepath.wstring();
+	wfilepath.erase(std::remove_if(wfilepath.begin(), wfilepath.end(), [](wchar_t c) { return !((c >= 0 && c < 128) || (c >= 0x400 && c < 0x500)); }), wfilepath.cend());
+	const std::filesystem::path filepath_ex{ wfilepath };
+	
+	const std::string fileName = filepath_ex.filename().string();
+
 	const std::filesystem::path fileFolder = filepath.parent_path();
-	const std::string fileName = filepath.filename().string();
-	m_stemFileName = filepath.stem().string();
 	m_mapFolder = fileFolder / "map.000";		// todo: add check for already converted files to not to 
 	m_misFolder = m_mapFolder / "mis.000";
 
@@ -205,7 +210,7 @@ void Converter::convertMap(const std::filesystem::path& filepath)
 	}
 	};
 
-	own::printlnSuccess(Dictionary::getValue(STRINGS::SUCCESS_CONVERTED), filepath.string(), m_mapFolder.string());
+	own::printlnSuccess(Dictionary::getValue(STRINGS::SUCCESS_CONVERTED), filepath_ex.string(), m_mapFolder.string());
 }
 
 uint32_t Converter::convertMapFileSMM(const std::string_view& inputFile)
@@ -1062,14 +1067,14 @@ void Converter::convertMisGroups(const std::string_view& mis_groups) const
 		
 		if (atime1 == 0 && delay1 == 255 && delay2 == 255 && delay3 == 255 && delay4 == 127 && min == 0 && force1 == 255 && force2 == 255 && force3 == 255 && force4 == 127)
 		{
-			outputFileMisGroups << " reserv auto=" << 0 << " delay=" << 0 << " min=" << 0 << " force=" << 0 << " atime=" << 0;
+			outputFileMisGroups << " reserv auto=0 delay=0 min=0 force=0 atime=0";
 		}
 		else
 			if (atime1 > 0 && delay1 == 255 && delay2 == 255 && delay3 == 255 && delay4 == 127 && min == 0 && force1 == 255 && force2 == 255 && force3 == 255 && force4 == 127)
 			{
 				uint32_t atime1234 = ((atime4 & 0xFF) << 24) | ((atime3 & 0xFF) << 16) | ((atime2 & 0xFF) << 8) | (atime1 & 0xFF);
 				uint32_t convertatime = atime1234 / timeconvertnum;
-				outputFileMisGroups << " reserv auto=" << 1 << " delay=" << 0 << " min=" << min << " force=" << 0 << " atime=" << convertatime;
+				outputFileMisGroups << std::format(" reserv auto=1 delay=0 min={} force=0 atime={}", min, convertatime);
 			}
 			else
 				if (atime1 > 0 && (delay3 >= 1 && delay3 <= 13) && min == 0 && force1 == 255 && force2 == 255 && force3 == 255 && force4 == 127)
@@ -1078,7 +1083,7 @@ void Converter::convertMisGroups(const std::string_view& mis_groups) const
 					uint32_t delay1234 = ((delay4 & 0xFF) << 24) | ((delay3 & 0xFF) << 16) | ((delay2 & 0xFF) << 8) | (delay1 & 0xFF);
 					uint32_t convertatime = atime1234 / timeconvertnum;
 					uint32_t convertdelay = delay1234 / timeconvertnum;
-					outputFileMisGroups << " reserv auto=" << 3 << " delay=" << convertdelay << " min=" << min << " force=" << 0 << " atime=" << convertatime;
+					outputFileMisGroups << std::format(" reserv auto=3 delay={} min={} force=0 atime={}", convertdelay, min, convertatime);
 				}
 				else
 					if (atime1 > 0 && (delay3 >= 1 && delay3 <= 13) && (min >= 1 && min <= 98) && force1 == 255 && force2 == 255 && force3 == 255 && force4 == 127)
@@ -1087,7 +1092,7 @@ void Converter::convertMisGroups(const std::string_view& mis_groups) const
 						uint32_t delay1234 = ((delay4 & 0xFF) << 24) | ((delay3 & 0xFF) << 16) | ((delay2 & 0xFF) << 8) | (delay1 & 0xFF);
 						uint32_t convertatime = atime1234 / timeconvertnum;
 						uint32_t convertdelay = delay1234 / timeconvertnum;
-						outputFileMisGroups << " reserv auto=" << 7 << " delay=" << convertdelay << " min=" << min << " force=" << 0 << " atime=" << convertatime;
+						outputFileMisGroups << std::format(" reserv auto=7 delay={} min={} force=0 atime={}", convertdelay, min, convertatime);
 					}
 					else
 						if (atime1 > 0 && (delay3 >= 1 && delay3 <= 13) && min == 0 && (force3 >= 1 && force3 <= 13))
@@ -1098,7 +1103,7 @@ void Converter::convertMisGroups(const std::string_view& mis_groups) const
 							uint32_t convertatime = atime1234 / timeconvertnum;
 							uint32_t convertdelay = delay1234 / timeconvertnum;
 							uint32_t convertforce = force1234 / timeconvertnum;
-							outputFileMisGroups << " reserv auto=" << 11 << " delay=" << convertdelay << " min=" << min << " force=" << convertforce << " atime=" << convertatime;
+							outputFileMisGroups << std::format(" reserv auto=11 delay={} min={} force={} atime={}", convertdelay, min, convertforce, convertatime);
 						}
 						else
 							if (atime1 > 0 && delay1 == 255 && delay2 == 255 && delay3 == 255 && delay4 == 127 && min == 0 && (force3 >= 1 && force3 <= 13))
@@ -1107,7 +1112,7 @@ void Converter::convertMisGroups(const std::string_view& mis_groups) const
 								uint32_t force1234 = ((force4 & 0xFF) << 24) | ((force3 & 0xFF) << 16) | ((force2 & 0xFF) << 8) | (force1 & 0xFF);
 								uint32_t convertatime = atime1234 / timeconvertnum;
 								uint32_t convertforce = force1234 / timeconvertnum;
-								outputFileMisGroups << " reserv auto=" << 9 << " delay=" << 0 << " min=" << 0 << " force=" << convertforce << " atime=" << convertatime;
+								outputFileMisGroups << std::format(" reserv auto=9 delay=0 min=0 force={} atime={}", convertforce, convertatime);
 							}
 							else
 								if (atime1 > 0 && delay1 == 255 && delay2 == 255 && delay3 == 255 && delay4 == 127 && (min >= 1 && min <= 98) && (force3 >= 1 && force3 <= 13))
@@ -1116,7 +1121,7 @@ void Converter::convertMisGroups(const std::string_view& mis_groups) const
 									uint32_t force1234 = ((force4 & 0xFF) << 24) | ((force3 & 0xFF) << 16) | ((force2 & 0xFF) << 8) | (force1 & 0xFF);
 									uint32_t convertatime = atime1234 / timeconvertnum;
 									uint32_t convertforce = force1234 / timeconvertnum;
-									outputFileMisGroups << " reserv auto=" << 13 << " delay=" << 0 << " min=" << min << " force=" << convertforce << " atime=" << convertatime;
+									outputFileMisGroups << std::format(" reserv auto=13 delay=0 min={} force={} atime={}", min, convertforce, convertatime);
 								}
 								else
 								{
@@ -1126,11 +1131,16 @@ void Converter::convertMisGroups(const std::string_view& mis_groups) const
 									uint32_t convertatime = atime1234 / timeconvertnum;
 									uint32_t convertdelay = delay1234 / timeconvertnum;
 									uint32_t convertforce = force1234 / timeconvertnum;
-									outputFileMisGroups << " reserv auto=" << 15 << " delay=" << convertdelay << " min=" << min << " force=" << convertforce << " atime=" << convertatime;
+									outputFileMisGroups << std::format(" reserv auto=15 delay={} min={} force={} atime={}", convertdelay, min, convertforce, convertatime);
 								}
 		
 		{
-			outputFileMisGroups << " flag=" << (uint16_t)flag << " zone=" << (uint16_t)zone << " hp=" << (uint16_t)hp << " ammo=" << (uint16_t)ammo << " expa=" << (uint16_t)expa << '\n';
+			outputFileMisGroups << std::format(" flag={} zone={} hp={} ammo={} expa={}\n"
+				, (uint16_t)flag
+				, (uint16_t)zone
+				, (uint16_t)hp
+				, (uint16_t)ammo
+				, (uint16_t)expa);
 		}
 		
 	}
